b98ab2a5208cc84947beac4731786f32026ac47c
dinghui <dh243542757@gmail.com>
2025-04-04 17:56:07 +0800

    aop异常通知支持自定义异常类型

--- src/main/java/com/dh/framework/aop/aspect/DhAspectJAfterThrowingInterceptor.java
+++ src/main/java/com/dh/framework/aop/aspect/DhAspectJAfterThrowingInterceptor.java
@@ -33,7 +33,7 @@ public class DhAspectJAfterThrowingInterceptor extends DhAbstractAspectJAdvice i
         } catch (Throwable ex) {
             log.info("throwType:{},ex:{}", throwType, ex.getClass());
             // 如果指定了异常类型，则只处理该类型的异常
-            if (this.throwType == null || this.throwType.isAssignableFrom(ex.getClass())) {
+            if (throwType == null || throwType.isAssignableFrom(ex.getClass())) {
                 invokeAdviceMethod(mi, null, ex);
             }
             throw ex;
@@ -48,14 +48,14 @@ public class DhAspectJAfterThrowingInterceptor extends DhAbstractAspectJAdvice i
     public void setThrowType(String typeName) {
         if (typeName != null && !typeName.isEmpty()) {
             try {
-                this.throwType = Class.forName(typeName);
-                if (!Throwable.class.isAssignableFrom(this.throwType)) {
+                throwType = Class.forName(typeName);
+                if (!Throwable.class.isAssignableFrom(throwType)) {
                     log.error("指定的类型 '" + typeName + "' 不是Throwable的子类");
-                    this.throwType = null;
+                    throwType = null;
                 }
             } catch (ClassNotFoundException e) {
                 log.error("无法加载指定的异常类型 '" + typeName + "'");
-                this.throwType = null;
+                throwType = null;
             }
         }
     }
@@ -64,6 +64,6 @@ public class DhAspectJAfterThrowingInterceptor extends DhAbstractAspectJAdvice i
      * 获取要捕获的异常类型
      */
     public Class<?> getThrowType() {
-        return this.throwType;
+        return throwType;
     }
 }

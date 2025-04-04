593577e6ba989fba5016f79c29adc05544addbe5
dinghui <dh243542757@gmail.com>
2025-04-04 22:40:47 +0800

    aop后置通知

--- src/main/java/com/dh/framework/aop/support/DhAdvisedSupport.java
+++ src/main/java/com/dh/framework/aop/support/DhAdvisedSupport.java
@@ -98,6 +98,22 @@ public class DhAdvisedSupport {
         List<Object> advices = new LinkedList<>();
         try {
             Object aspectInstance = aspectClass.newInstance();
+            // 添加环绕通知
+            String aroundMethod = config.getAspectAround();
+            if (isValidMethod(aroundMethod)) {
+                Method aspectMethod = aspectMethods.get(aroundMethod);
+                if (aspectMethod != null) {
+                    advices.add(new DhMethodAroundAdviceInterceptor(aspectInstance, aspectMethod));
+                }
+            }
+            // 添加后置通知
+            String afterMethod = config.getAspectAfter();
+            if (isValidMethod(afterMethod)) {
+                Method aspectMethod = aspectMethods.get(afterMethod);
+                if (aspectMethod != null) {
+                    advices.add(new DhAfterAdviceInterceptor(aspectInstance, aspectMethod));
+                }
+            }
             // 添加返回通知
             String afterReturnMethod = config.getAspectAfterReturn();
             if (isValidMethod(afterReturnMethod)) {
@@ -126,22 +142,6 @@ public class DhAdvisedSupport {
                         advice.setThrowType(throwType);
                     }
                     advices.add(advice);
-                }
-            }
-            // 添加后置通知
-            String afterMethod = config.getAspectAfter();
-            if (isValidMethod(afterMethod)) {
-                Method aspectMethod = aspectMethods.get(afterMethod);
-                if (aspectMethod != null) {
-                    advices.add(new DhAfterAdviceInterceptor(aspectInstance, aspectMethod));
-                }
-            }
-            // 添加环绕通知
-            String aroundMethod = config.getAspectAround();
-            if (isValidMethod(aroundMethod)) {
-                Method aspectMethod = aspectMethods.get(aroundMethod);
-                if (aspectMethod != null) {
-                    advices.add(new DhMethodAroundAdviceInterceptor(aspectInstance, aspectMethod));
                 }
             }
             if (!advices.isEmpty()) {

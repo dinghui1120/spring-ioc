57b74ae0e08aafc4262d66793da0c65cf97dc161
dinghui <dh243542757@gmail.com>
2025-04-06 21:29:09 +0800

    aop属性使用ThreadLocal

--- src/main/java/com/dh/framework/aop/aspect/interceptor/DhThreadLocalCleanupInterceptor.java
+++ src/main/java/com/dh/framework/aop/aspect/interceptor/DhThreadLocalCleanupInterceptor.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * ThreadLocal资源清理拦截器
  * 用于在所有通知执行完毕后清理ThreadLocal资源
- * 应作为拦截器链中的最后一个执行
  */
 @Slf4j
 public class DhThreadLocalCleanupInterceptor implements DhMethodInterceptor {
@@ -18,7 +17,6 @@ public class DhThreadLocalCleanupInterceptor implements DhMethodInterceptor {
         try {
             return mi.proceed();
         } finally {
-            // 所有通知都已执行完毕，现在可以安全地清理ThreadLocal资源
             // 只在最外层调用时清理，避免嵌套调用问题
             if (mi.isRootInvocation()) {
                 log.debug("正在清理ThreadLocal资源，方法: {}, 属性: {}", 
